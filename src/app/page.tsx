5ba6a4c169bebdf5bf4b8d3f1d89f58296fcf66f
Oyunbat <oyunbat9958@gmail.com>
2025-04-16 19:43:20 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Admin from "./admin/admin";
 import Customer from "./customer/customer";
+import Info from "./loginInfo/Information";
 export default function Home() {
   return (
     <>
       <div>
         {/* <Admin /> */}
-        <Customer />
+        {/* <Customer /> */}
+        <Info setIsLogin={() => {}} />
       </div>
     </>
   );
